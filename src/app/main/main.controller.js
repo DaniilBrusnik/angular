29abedb224a6a631961efce2224784661752a029
Daniil <brusvik@ukr.net>
2016-10-13 09:57:19 +0300

    2

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,143 +1,148 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('test')
-    .controller('MainController', MainController);
-
-
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr, $scope) {
-
-    var vm = this;
+    angular
+      .module('test')
+      .controller('MainController', MainController);
 
 
-    vm.array = [
-      {
-        "id": "rule",
-        "if": [
-          {
-            id_rule: 'NewRule1',
-            "type": "condition",
-            "target": "Carrier",
-            "payment": "CPA",
-            "compare": "Greater then",
-            "value": ''
-          }
-        ],
-        "then": "black"
-      }
-    ];
+    /** @ngInject */
+    function MainController($timeout, webDevTec, toastr, $scope) {
 
-    vm.addNewRule = function () {
+      var vm = this;
 
-    }
 
-    vm.addNewFields = function (rule) {
-      var newItemNo = vm.array.length + 1;
-      rule.push(
-        {"id_logic": "NewRule" + newItemNo, "type": "logic", "logicOrAnd": true},
-        {"id_rule": "NewRule" + newItemNo, "type": "condition", "target": "placement", "compare": "<", "value": ""}
-      );
-
-    };
-
-    vm.addNewFieldsToGroups = function (data) {
-      var newItemNo = vm.array.length + 1;
-      data.push(
-        {"id_logic": "NewRule" + newItemNo, "type": "logic", "logicOrAnd": true},
-        {"id_rule": "NewRule" + newItemNo, "type": "condition", "target": "placement", "compare": "<", "value": ""}
-      );
-
-    };
-    vm.addNewGroupFields = function (rule) {
-      var newItemNo = vm.array.length + 1;
-      rule.push(
-        {"id_logic": "NewRule",
-          "type": "logic",
-          "logicOrAnd": true
-        },
-        { "type": "group",
-          "nodes": [
+      vm.array = [
+        {
+          "id": "rule",
+          "if": [
             {
               id_rule: 'NewRule1',
               "type": "condition",
-              "target": "Carrier",
-              "payment": "CPA",
-              "compare": "Greater then",
+              "target": "",
+              "payment": "",
+              "compare": "",
               "value": ''
-            }
-          ]
-        }
-      );
-      console.log(vm.array);
-    };
-    vm.addNewRule = function () {
-      var newItemNo = vm.array.length + 1;
-
-      vm.array.push(
-        {
-          "id": "rule" + newItemNo,
-          "if": [
-            {
-              "id_rule": "NewRule" + newItemNo,
-              "type": "condition",
-              "target": "placement",
-              "compare": "<",
-              "value": ""
             }
           ],
           "then": "black"
         }
-      );
-      console.log(vm.array);
-    };
+      ];
+
+      vm.addField = function (rule) {
+        if (rule.$parent.$parent.$parent.$parent.rule) {
+          var newItemNo = vm.array.length + 1;
+          rule.$parent.$parent.$parent.$parent.rule.push(
+            {"id_logic": "NewRule" + newItemNo, "type": "logic", "logicOrAnd": true},
+            {"id_rule": "NewRule" + newItemNo, "type": "condition", "target": "placement", "compare": "<", "value": ""}
+          );
+
+        } else {
+          var newItemNo = vm.array.length + 1;
+          rule.$parent.$parent.rules.if.push(
+            {"id_logic": "NewRule" + newItemNo, "type": "logic", "logicOrAnd": true},
+            {"id_rule": "NewRule" + newItemNo, "type": "condition", "target": "placement", "compare": "<", "value": ""}
+          );
+        }
+      };
+
+      vm.addGroup = function (rule, ind) {
+        if (rule.$parent.$parent.$parent.rule) {
+          var newItemNo = vm.array.length + 1;
+          rule.$parent.$parent.$parent.rule.push({
+              "id_logic": "NewRule" + newItemNo,
+              "type": "logic",
+              "logicOrAnd": true
+            },
+            [
+              {
+                id_rule: 'NewGroup' + newItemNo,
+                "type": "condition",
+                "target": "Carrier",
+                "payment": "CPA",
+                "compare": ">",
+                "value": ''
+              }
+            ]
+          );
+        } else {
+          var newItemNo = vm.array.length + 1;
+          rule.$parent.rules.if.push({"id_logic": "NewRule" + newItemNo, "type": "logic", "logicOrAnd": true},
+            [
+              {
+                id_rule: 'NewGroup' + newItemNo,
+                "type": "condition",
+                "target": "Carrier",
+                "payment": "CPA",
+                "compare": ">",
+                "value": ''
+              }
+            ]
+          );
+        }
+      };
+
+      vm.addNewRule = function () {
+        var newItemNo = vm.array.length + 1;
+        vm.array.push(
+          {
+            "id": "rule" + newItemNo,
+            "if": [
+              {
+                "id_rule": "NewRule" + newItemNo,
+                "type": "condition",
+                "target": "placement",
+                "compare": "<",
+                "value": ""
+              }
+            ],
+            "then": "black"
+          }
+        );
+      };
 
 
-    vm.deleteNewlogic = function (ind) {
+      vm.deleteRule = function (ind) {
+        vm.array.splice(ind, 1);
+      };
 
-      vm.array.splice(ind, 1);
+      vm.deleteFilds = function (rule, ind) {
+        /*rule.splice(ind, 2);*/
+        if (rule.$parent.$parent.$parent.$parent.rule) {
+          rule.$parent.$parent.$parent.$parent.rule.splice(ind, 2);
+        } else {
+          rule.$parent.$parent.rules.if.splice(ind, 2);
+        }
+      };
 
-    };
+      vm.test = function (rule) {
+        console.log(rule);
+      };
 
-    vm.deleteNewfilds = function (ruleIf, ind) {
-      ruleIf.splice(ind, 2);
+      vm.typeOfLogic = function (rule) {
+        if (rule.type === 'logic') {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
-    };
+      vm.typeOfObject = function (rule) {
+        if (rule.type == 'condition') {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
+      vm.typeOfArray = function (rule) {
+        if (Array.isArray(rule) == true) {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
-    vm.addlist = function () {
-    };
-    vm.ChoiceB = function () {
-
-      alert('condition');
-    };
-
-
-    vm.typeOfLogic = function (rule) {
-      if (rule.type === 'logic') {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-
-    vm.typeOfObject = function (rule) {
-      if (rule.type == 'condition') {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    vm.typeOfGroup = function (rule) {
-      if (rule.type == 'group') {
-        return true;
-      } else {
-        return false;
-      }
-    };
+    }
   }
-
-})();
+  )();
